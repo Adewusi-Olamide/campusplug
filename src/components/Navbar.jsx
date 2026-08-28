e6da963c50e9195478570cf330ca7fb234f6ab4f
aycode7 <159772484+aycode7@users.noreply.github.com>
2026-08-28 01:50:17 +0100

    still working on navbar when john made me put an image folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,40 @@
+'use client'
+import { useState } from 'react'
 import React from 'react'
 import Link from 'next/link'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
-        <h1>Navbar</h1>
+
+        <div className='navbar'>
+
+          <div className="left">
+            <h1>CampusPlug</h1>
+          </div>
+
+          <div className="center">
+            <p>Home</p>
+            <p>Tools </p>
+            <p>Resources</p>
+            <p>Blog</p>
+            <p>About Us</p>
+          </div>
+
+          <div className="right">
+
+            <div className='search'>
+              <input type='text' placeholder='Search tools...' />
+              <p>⨀</p>
+            </div>
+
+            <p>⨀</p>
+            <button>Sign In</button>
+
+          </div>
+
+        </div>
       
     </div>
   )
