4aa86ef831a2ac46023bc7ed60750e768237ccaa
Adewusi-Olamide <ademiposiadewusi@gmail.com>
2026-08-30 11:05:32 -0700

    resources page update

--- src/app/resources/page.jsx
+++ src/app/resources/page.jsx
@@ -10,6 +10,13 @@ import { useEffect } from 'react'
 import { Notebook } from 'lucide-react'
 import { Lightbulb } from 'lucide-react'
 import { NotebookPen } from 'lucide-react'
+import { BookOpenText } from 'lucide-react'
+import { Layers } from 'lucide-react'
+import { Gift } from 'lucide-react'
+import { Star } from 'lucide-react'
+import { Globe } from 'lucide-react'
+import { CircleAlert } from 'lucide-react'
+import { ShieldCheck } from 'lucide-react'
 
 const page = () => {
 
@@ -56,6 +63,122 @@ const page = () => {
           <div className="right">
             <Image className="image" src="/images/books.png" width={1000} height={1000} alt='homepage' />
           </div>
+        </div>
+
+        <div className="container-2">
+
+          <div className="head">
+            <h1>All Resources</h1>
+          </div>
+
+          {/* first content */}
+
+          <div className="content">
+
+            <div className="syllabus">
+              <p className="syllabus-img"><Notebook size={30} /></p>
+              <h3>JAMB Syllabus</h3>
+              <p> Official UTME syllabus for all subjects. Know  what to study and what to expect.</p>
+              <div className="button">
+                <button className="left">Syllabus</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="guide-img"><GraduationCap size={35} /></p>
+              <h3>UTME Study Guides</h3>
+              <p>Topic-by-topic study notes and explanations to help you  understand better.</p>
+              <div className="button">
+                <button className="left">Study Guides</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="flashcard-img"><Layers size={30} /></p>
+              <h3>Flashcards</h3>
+              <p>Flashcards improve memory, boost recalls, and make studying faster and easier</p>
+              <div className="button">
+                <button className="left">Flashcards</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="tips-img"><Lightbulb size={30} /></p>
+              <h3>Exam Preparatin Tips</h3>
+              <p>Smart strategies, time management and CBT tips to excel in your exams.</p>
+              <div className="button">
+                <button className="left">Preparation tips</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+          </div>
+
+          {/* second content */}
+
+          <div className="content">
+
+            <div className="syllabus">
+              <p className="textbook-img"><BookOpenText size={30} /></p>
+              <h3>Recommended Textbooks</h3>
+              <p>Best textbook for each subject recommended by top students and teachers.</p>
+              <div className="button">
+                <button className="left">Textbooks</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="tips-img"><Star size={30} /></p>
+              <h3>Subject Revision Notes</h3>
+              <p>Quick revision notes and key formulas to help you revise faster and smarter.</p>
+              <div className="button">
+                <button className="left">Revision</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="textbook-img"><Gift size={30} /></p>
+              <h3>Scholarships in Nigeria</h3>
+              <p>Latest  local and international scholarship for Nigerian students and applicants.</p>
+              <div className="button">
+                <button className="left">Scholarships</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+            <div className="syllabus">
+              <p className="syllabus-img"><Globe size={30} /></p>
+              <h3>Official JAMB Resources</h3>
+              <p>Important links to the official JAMB servicees, portals and helpfuul information.</p>
+              <div className="button">
+                <button className="left">Study Guides</button>
+                <button className="right">→</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="container-3">
+
+          <div className="left">
+            <p className="alert"><CircleAlert size={50} /></p>
+
+            <div className="notice">
+              <h1>Important notice</h1>
+              <p>CampusPlug does not provide official JAMB past questions. We provide original practice questions, <br /> study guides, and resources to help prepare better for Exams "UTME included".</p>
+            </div>
+          </div>
+
+          <div className="right">
+            <p  className="check"><ShieldCheck size={60} /></p>
+          </div>
+
         </div>
       </div>
       
